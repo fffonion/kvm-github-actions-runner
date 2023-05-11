941a879bcae645e1218eba42c077428b82e9d07f
Wangchong Zhou <fffonion@gmail.com>
2023-05-11 14:08:18 +0800

    add bind_host

--- provision/setup-datadog.sh
+++ provision/setup-datadog.sh
@@ -2,7 +2,8 @@
 
 # wget
 # update bind_host to bridge
-sed -i.bak 's/.*logs_enabled:.*/logs_enabled: true/g' /etc/datadog-agent/datadog.yaml
+sed -i.bak 's/.*bind_host:.*/bind_host: 10.1.0.1/' /etc/datadog-agent/datadog.yaml
+sed -i.bak 's/.*logs_enabled:.*/logs_enabled: true/' /etc/datadog-agent/datadog.yaml
 sed -i.bak 's/.*histogram_aggregates:.*/histogram_aggregates: ["max","medium","avg","count","sum"]/' /etc/datadog-agent/datadog.yaml
 
 cat << EOF > /etc/datadog-agent/conf.d/self-hosted.yml
