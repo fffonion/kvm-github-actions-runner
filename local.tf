e23d1e4977768227277d05bce6ca320dbe5ba996
Wangchong Zhou <fffonion@gmail.com>
2024-01-16 11:14:31 +0800

    bump 20240107.1

--- local.tf
+++ local.tf
@@ -1,5 +1,5 @@
 locals {
-  previous_image_version = "20231126.2"
-  image_version          = "20231217.2"
+  previous_image_version = "20231217.2"
+  image_version          = "20240107.1"
   runner_version         = "2.311.0"
 }
