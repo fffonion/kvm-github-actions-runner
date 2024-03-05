23b02c853885318286f16fa5e41d224e41f6069b
Wangchong Zhou <wangchong@konghq.com>
2024-03-05 17:44:43 +0800

    adjust arm64 image sync

--- ops/sync_images.sh
+++ ops/sync_images.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 total_runners=${1:-13}
 image_version=$(cat $(dirname $0)/../local.tf|grep image_version|grep -v previous|cut -d '"' -f2)
-image_version=${2:-$image_version}
+image_arch=${2:-amd64}
+image_version=${3:-$image_version}
 file_name=/root/ubuntu-22.04-$image_version
 
 if [[ -z "$RUNNER_IMAGE_STORAGE_HOST" ]]; then
@@ -9,23 +10,29 @@ if [[ -z "$RUNNER_IMAGE_STORAGE_HOST" ]]; then
     exit 1
 fi
 
-if [[ $from == *a ]]; then
-    prefix="arm64-"
-fi
-
 echo "* Image to copy: $file_name"
-echo "* Bootstrap node: $from"
+echo "* Architecture: $image_arch"
 echo "* Total runners: $total_runners"
 
 function do_copy() {
-    ssh $dst sudo -E -H rsync -cha rsync://$RUNNER_IMAGE_STORAGE_HOST/build/$(basename $file_name) $file_name
+    dst=$1
+    if [[ $image_arch == "arm64" ]]; then
+        path=arm64/
+    else
+        path=
+    fi
+    ssh $dst sudo -E -H rsync -cha rsync://$RUNNER_IMAGE_STORAGE_HOST/build/$path$(basename $file_name) $file_name
     echo "+ Finished $dst"
 }
 
 for i in $(seq 1 $total_runners); do
-    dst="${prefix}runner-$i"
+    if [[ $image_arch == "arm64" ]]; then
+        dst="runner-arm64-$i"
+    else
+        dst="runner-$i"
+    fi
     echo "- Copying $dst"
-    do_copy $src $dst &
+    do_copy $dst &
 done
 
 echo "- Wait for copying to finish"
