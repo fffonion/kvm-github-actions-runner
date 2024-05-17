c8b1b84b8abfcd220c59e722aed1829304c55da2
Wangchong Zhou <fffonion@gmail.com>
2024-05-17 13:31:42 +0800

    Reapply "use cached runner package from image" as it broke on arm64
    This reverts commit 4b3cc46ec4ac8f90ee512f5c6f6333c07757c013.

--- local.tf
+++ local.tf
@@ -1,5 +1,4 @@
 locals {
   previous_image_version = "20240422.1"
   image_version          = "20240514.2"
-  runner_version         = "2.314.1"
 }
--- variables.tf
+++ variables.tf
@@ -22,11 +22,6 @@ variable "token" {
   type = string
 }
 
-variable "runner_version" {
-  type    = string
-  default = "2.301.1"
-}
-
 variable "docker_user" {
   type    = string
   default = ""
