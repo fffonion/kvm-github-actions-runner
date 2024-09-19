fcacbe940a27e845dc42363156fc7ca30c234199
Water-Melon <melon-c@hotmail.com>
2024-09-19 22:34:32 +0800

    bump image to 20240916.115

--- local.tf
+++ local.tf
@@ -1,5 +1,5 @@
 locals {
-  previous_image_version = "20240804.1"
-  image_version          = "20240811.3"
+  previous_image_version = "20240811.3"
+  image_version          = "20240916.115"
   runner_version         = "2.319.1"
 }
