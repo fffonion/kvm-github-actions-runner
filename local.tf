b303f176a11dd0c1d6d9ff6c90a922bfefc62409
Wangchong Zhou <wangchong@konghq.com>
2024-11-29 15:27:43 +0800

    bump image to 20241117.1

--- local.tf
+++ local.tf
@@ -1,5 +1,5 @@
 locals {
-  previous_image_version = "20241103.1"
-  image_version          = "20241103.2"
+  previous_image_version = "20241103.2"
+  image_version          = "20241117.1"
   runner_version         = "2.320.0"
 }
