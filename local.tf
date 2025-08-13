17cff73a6a589526c72ed71b025bde79d984c432
Water-Melon <melon-c@hotmail.com>
2025-08-13 17:18:32 +0800

    Revert "chore: bump image to 20250804.2 (#12)"
    This reverts commit 90e9e8646246aa1db0d1ead38eb7b33b88fc587b.

--- local.tf
+++ local.tf
@@ -1,5 +1,5 @@
 locals {
-  previous_image_version = "20250629.1"
-  image_version          = "20250804.2"
-  runner_version         = "2.327.1"
+  previous_image_version = "20250504.1"
+  image_version          = "20250629.1"
+  runner_version         = "2.325.0"
 }
